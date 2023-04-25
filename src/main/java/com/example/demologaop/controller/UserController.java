f28a0b22e8c432d4d1b471384ad5cde553d3dc55
26877 <2687776836@qq.com>
2023-04-25 13:36:59 +0800

    自定义@MyAnnotation使用

--- src/main/java/com/example/demologaop/controller/UserController.java
+++ src/main/java/com/example/demologaop/controller/UserController.java
@@ -3,6 +3,10 @@ package com.example.demologaop.controller;
 import com.example.demologaop.annotation.MyAnnotation;
 
 public class UserController {
+    /**
+     * 使用注解@MyAnnotation
+     * @return
+     */
    @MyAnnotation(name="张三",age=18,hobby = {"跑步","篮球"})
     public String get(){
         return "hello Annotation!";
